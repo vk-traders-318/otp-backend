6ca4bdc2a623c1e87c7d8c42effde07b075ed98c
vk-traders-318 <vktraders.office.1@gmail.com>
2026-05-03 14:27:36 +0530

    Update and rename index.js to server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const cors = require("cors");
+const dotenv = require("dotenv");
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", require("./routes/authRoutes"));
+
+app.listen(process.env.PORT || 5000, () => {
+  console.log("Server running");
+});
