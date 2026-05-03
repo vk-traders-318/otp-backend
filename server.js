d2d131151e6db9f5a65dfe201c20e82ba09aa10f
vk-traders-318 <vktraders.office.1@gmail.com>
2026-05-03 14:50:46 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,21 +3,30 @@ const cors = require("cors");
 const dotenv = require("dotenv");
 const connectDB = require("./config/db");
 
+// Load env variables
 dotenv.config();
+
+// Connect Database
+connectDB();
 
 const app = express();
 
+// Middlewares
 app.use(cors());
 app.use(express.json());
 
-connectDB();
-
+// Routes
 app.use("/api/auth", require("./routes/authRoutes"));
 
+// Test Route
 app.get("/", (req, res) => {
-  res.send("OTP Backend Running");
+  res.send("OTP Backend Running 🚀");
 });
 
-app.listen(process.env.PORT, () => {
-  console.log("Server running on port " + process.env.PORT);
+// PORT FIX (IMPORTANT 🔥)
+const PORT = process.env.PORT || 5000;
+
+// Start Server
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
